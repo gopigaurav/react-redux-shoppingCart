b25adcae38f34cd7f109733e25ff459fcfb1d738
gopi <gopigaurav9@gmail.com>
2021-05-17 13:28:04 +0530

    Updates

---
+++ precache-manifest.6b3aa4b660c3ff0655b922936d3d55b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999d3698cd27c8fad38829b55b80c1f4",
+    "url": "/react-redux-shoppingCart/index.html"
+  },
+  {
+    "revision": "a1ef4d598884937a7423",
+    "url": "/react-redux-shoppingCart/static/css/main.ecd18c60.chunk.css"
+  },
+  {
+    "revision": "4c0248de15b2dac8d772",
+    "url": "/react-redux-shoppingCart/static/js/2.18ae7d22.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/react-redux-shoppingCart/static/js/2.18ae7d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ef4d598884937a7423",
+    "url": "/react-redux-shoppingCart/static/js/main.7beddbaf.chunk.js"
+  },
+  {
+    "revision": "38c6be79c6e17ad51456",
+    "url": "/react-redux-shoppingCart/static/js/runtime-main.fa874019.js"
+  }
+]);
